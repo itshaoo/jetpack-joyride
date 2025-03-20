c8f90df54e5d28d382337db8dd71baab894f7293
hao <t111590062@ntut.org.tw>
2025-03-20 19:54:03 +0800

    add barry animation

---
+++ include/Animation.hpp
@@ -0,0 +1,47 @@
+#ifndef ANIMATION_HPP
+#define ANIMATION_HPP
+
+#include <string>
+#include <vector>
+
+#include "Util/Animation.hpp"
+#include "Util/GameObject.hpp"
+
+
+class Animation : public Util::GameObject {
+
+public:
+    explicit Animation(const std::vector<std::string>& AnimationPaths);
+
+    [[nodiscard]] bool IsLooping() const {
+        return std::dynamic_pointer_cast<Util::Animation>(m_Drawable)->GetLooping();
+    }
+
+    [[nodiscard]] bool IsPlaying() const {
+        return std::dynamic_pointer_cast<Util::Animation>(m_Drawable)->GetState() == Util::Animation::State::PLAY;
+    }
+
+    void SetPosition(const glm::vec2& Position) { m_Transform.translation = Position; }
+
+    [[nodiscard]] const glm::vec2& GetPosition() const { return m_Transform.translation; }
+
+    void SetLooping(bool looping) {
+        auto temp = std::dynamic_pointer_cast<Util::Animation>(m_Drawable);
+        temp->SetLooping(looping);
+    }
+
+    void SetInterval(int interval) {
+        auto temp = std::dynamic_pointer_cast<Util::Animation>(m_Drawable);
+        temp->SetInterval(interval);
+    }
+
+    void Play() {
+        auto temp = std::dynamic_pointer_cast<Util::Animation>(m_Drawable);
+        temp->Play();
+    }
+
+    [[nodiscard]] bool IfAnimationEnds() const;
+
+};
+
+#endif //ANIMATION_HPP
--- src/App.cpp
+++ src/App.cpp
@@ -7,7 +7,6 @@
 
 void App::Start() {
     LOG_TRACE("Start");
-    m_CurrentState = State::UPDATE;
 
     m_BackgroundImage = std::make_shared<BackgroundImage>();
     m_Root.AddChild(m_BackgroundImage);
@@ -18,10 +17,60 @@ void App::Start() {
     m_icon->SetPosition({centerX, centerY});
     m_icon->SetZIndex(50);
     m_Root.AddChild(m_icon);
+
+    m_moveIcon = false; 
+    m_iconOutOfWindow = false;
+
+    // Create the Barry animation
+    std::vector<std::string> barryPaths = {
+        RESOURCE_DIR"/Image/barry/barry0.png",
+        RESOURCE_DIR"/Image/barry/barry1.png"
+    };
+    m_Barry = std::make_shared<Animation>(barryPaths);
+    m_Barry->SetPosition({-220.5f, -265.5f});
+    m_Barry->SetLooping(true);
+    m_Barry->SetInterval(100);
+    m_Barry->Play();
+    m_Root.AddChild(m_Barry);
+
+    m_CurrentState = State::UPDATE;
+
 }
 
 void App::Update() {
-    //TODO: do your things here and delete this line <3
+    if (Util::Input::IsKeyPressed(Util::Keycode::MOUSE_LB) || Util::Input::IsKeyPressed(Util::Keycode::SPACE)) {
+        m_moveIcon = true;
+    }
+
+    // Move the icon upwards if the flag is set
+    if (m_moveIcon) {
+        glm::vec2 currentPosition = m_icon->GetPosition();
+        currentPosition.y += 10.0f; // Move the icon upwards
+        m_icon->SetPosition(currentPosition);
+
+        // Stop moving the icon if it is out of the window
+        if (currentPosition.y + m_icon->GetScaledSize().y < 0) {
+            m_moveIcon = false;
+            m_iconOutOfWindow = true; 
+        }
+    }
+
+    if (m_iconOutOfWindow) {
+        if (Util::Input::IsKeyPressed(Util::Keycode::MOUSE_LB) || Util::Input::IsKeyPressed(Util::Keycode::SPACE)) {
+            // Move Barry upwards
+            glm::vec2 barryPosition = m_Barry->GetPosition();
+            barryPosition.y += 10.0f;
+            m_Barry->SetPosition(barryPosition);
+        } else {
+            // Return Barry to its original position
+            glm::vec2 originalPosition = {-220.5f, -265.5f};
+            glm::vec2 barryPosition = m_Barry->GetPosition();
+            if (barryPosition.y < originalPosition.y) {
+                barryPosition.y -= 7.0f;
+                m_Barry->SetPosition(barryPosition);
+            }
+        }
+    }
     /*
      * Do not touch the code below as they serve the purpose for
      * closing the window.
