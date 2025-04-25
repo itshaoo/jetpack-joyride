0e3be699ffdd34eb872e828f772d8950fa1eda40
hao <t111590062@ntut.org.tw>
2025-04-25 15:32:33 +0800

    test

--- include/Missile.hpp
+++ include/Missile.hpp
@@ -5,6 +5,7 @@
 #include <Util/Renderer.hpp>
 #include <Util/Time.hpp>
 #include <Animation.hpp>
+#include <Util/BGM.hpp>
 
 class Missile {
 public:
@@ -12,7 +13,7 @@ public:
 
     void SetTargetPosition(const glm::vec2& targetPosition);
     void SetPosition(const glm::vec2& position);
-    void SetBarryPositionPtr(const glm::vec2* barryPositionPtr); // New method to set Barry's position pointer
+    void SetBarryPositionPtr(const glm::vec2* barryPositionPtr);  // New method to set Barry's position pointer
     void AddToRenderer(Util::Renderer& renderer);
     void Launch();
 
@@ -24,6 +25,9 @@ public:
                                Util::Renderer& renderer,
                                const glm::vec2& barryPosition);
 
+    glm::vec2 GetPosition() const { return m_Position; }
+    glm::vec2 GetSize()     const { return missileAnimation->GetScaledSize(); }
+
 private:
     glm::vec2 m_Position = {650.0f, 0.0f};
     glm::vec2 m_TargetPosition{};
@@ -31,8 +35,13 @@ private:
     float m_TrackingTime = 1000.0f; 
     float m_TrackingTimer = 0.0f;
 
-    float m_Speed = 10.0f;
+    float m_Speed = 15.0f;
     bool m_Launched = false;
+    std::shared_ptr<Util::BGM> missileSound;
+    std::shared_ptr<Util::BGM> warningSound;
+
+    bool m_WarningSoundPlayed = false;
+    bool m_SoundPlayed = false;
 
     std::shared_ptr<Animation> missileAnimation;
     std::shared_ptr<Animation> warningAnimation;
